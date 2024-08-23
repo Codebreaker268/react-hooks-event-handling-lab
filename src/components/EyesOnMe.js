c8c1d76916db27b7348ff1e8a6242fe570e4af55
Godfrey Onyango <godiodhis268@gmail.com>
2024-08-23 11:38:55 +0300

    only

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function handleFocus(event){
+        console.log("Good!",event.target.value)
+
+    }
+    function handleBlur(e){
+        console.log("Hey! Eyes on me!",e.target.value)
+    }
+    return(
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+
+}
+export default EyesOnMe;
